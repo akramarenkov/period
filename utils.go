9accbc5a80ee92b076a7ad0cdbd77b3b929d539b
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2023-06-23 00:48:03 +0300

    unworked safe multiplication

--- utils.go
+++ utils.go
@@ -1,6 +1,8 @@
 package period
 
-import "golang.org/x/exp/constraints"
+import (
+	"golang.org/x/exp/constraints"
+)
 
 func safeSum[Type constraints.Integer](first Type, second Type) (Type, bool) {
 	var zero Type
@@ -19,4 +21,47 @@ func safeSum[Type constraints.Integer](first Type, second Type) (Type, bool) {
 	}
 
 	return sum, false
+}
+
+func safeSlowMultiplication[Type constraints.Integer](first Type, second Type) (Type, bool) {
+	var zero Type
+
+	product := zero
+
+	multiplier := second
+
+	if second < zero {
+		multiplier = -second
+	}
+
+	for id := zero; id < multiplier; id++ {
+		sum, overflow := safeSum(product, first)
+		if overflow {
+			return zero, true
+		}
+
+		product = sum
+	}
+
+	if second < zero {
+		product = -product
+	}
+
+	return product, false
+}
+
+func safeMultiplication[Type constraints.Integer](first Type, second Type) (Type, bool) {
+	var zero Type
+
+	product := first * second
+
+	if second == zero {
+		return product, false
+	}
+
+	if product/second != first {
+		return zero, true
+	}
+
+	return product, false
 }
--- utils_test.go
+++ utils_test.go
@@ -209,3 +209,178 @@ func TestSafeSumUint8(t *testing.T) {
 	require.Equal(t, uint8(0), sum)
 	require.Equal(t, true, overflow)
 }
+
+func TestSafeSlowMultiplicationInt8(t *testing.T) {
+	product, overflow := safeSlowMultiplication[int8](0, 0)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](2, 0)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](0, 2)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](-2, 0)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](0, -2)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](3, 0)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](0, 3)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](-3, 0)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](0, -3)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](2, -3)
+	require.Equal(t, int8(-6), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](-3, 2)
+	require.Equal(t, int8(-6), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](-3, -2)
+	require.Equal(t, int8(6), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](127, 1)
+	require.Equal(t, int8(127), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](-128, 1)
+	require.Equal(t, int8(-128), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](127, 2)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, true, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](-128, 2)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, true, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](64, 3)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, true, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](-64, 3)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, true, overflow)
+
+	product, overflow = safeSlowMultiplication[int8](-128, -128)
+	require.Equal(t, int8(0), product)
+	require.Equal(t, true, overflow)
+}
+
+func TestSafeSlowMultiplicationUint8(t *testing.T) {
+	product, overflow := safeSlowMultiplication[uint8](0, 0)
+	require.Equal(t, uint8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](2, 0)
+	require.Equal(t, uint8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](0, 2)
+	require.Equal(t, uint8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](3, 0)
+	require.Equal(t, uint8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](0, 3)
+	require.Equal(t, uint8(0), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](2, 3)
+	require.Equal(t, uint8(6), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](3, 2)
+	require.Equal(t, uint8(6), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](127, 2)
+	require.Equal(t, uint8(254), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](255, 1)
+	require.Equal(t, uint8(255), product)
+	require.Equal(t, false, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](128, 2)
+	require.Equal(t, uint8(0), product)
+	require.Equal(t, true, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](127, 3)
+	require.Equal(t, uint8(0), product)
+	require.Equal(t, true, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](255, 2)
+	require.Equal(t, uint8(0), product)
+	require.Equal(t, true, overflow)
+
+	product, overflow = safeSlowMultiplication[uint8](255, 255)
+	require.Equal(t, uint8(0), product)
+	require.Equal(t, true, overflow)
+}
+
+func TestSafeMultiplicationUint8(t *testing.T) {
+	variants := 0
+
+	for first := 0; first <= 255; first++ {
+		for second := 0; second <= 255; second++ {
+			first8 := uint8(first)
+			second8 := uint8(second)
+
+			product, overflow := safeMultiplication[uint8](first8, second8)
+			referenceProduct, referenceOverflow := safeSlowMultiplication[uint8](first8, second8)
+
+			require.Equal(t, referenceProduct, product)
+			require.Equal(t, referenceOverflow, overflow)
+
+			variants++
+		}
+	}
+
+	require.Equal(t, 65536, variants)
+}
+
+/*
+func TestSafeMultiplicationInt8(t *testing.T) {
+	variants := 0
+
+	for first := -128; first <= 127; first++ {
+		for second := -128; second <= 127; second++ {
+			first8 := int8(first)
+			second8 := int8(second)
+
+			product, overflow := safeMultiplication[int8](first8, second8)
+			referenceProduct, referenceOverflow := safeSlowMultiplication[int8](first8, second8)
+
+			require.Equal(t, referenceProduct, product)
+			require.Equal(t, referenceOverflow, overflow, "%v * %v = %v (%v)", first, second, product, referenceProduct)
+
+			variants++
+		}
+	}
+
+	require.Equal(t, 65536, variants)
+}*/
