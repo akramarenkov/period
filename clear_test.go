6bfa42e1bea0735795f9bd8e9219b0d6c38d12f6
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2023-07-10 16:55:14 +0300

    simply tests

--- clear_test.go
+++ clear_test.go
@@ -7,54 +7,54 @@ import (
 )
 
 func TestClearInteger(t *testing.T) {
-	require.Equal(t, "", string(clearInteger("")))
-	require.Equal(t, "0", string(clearInteger("0")))
-	require.Equal(t, "0", string(clearInteger("00")))
-	require.Equal(t, "1", string(clearInteger("1")))
-	require.Equal(t, "12", string(clearInteger("12")))
-	require.Equal(t, "12", string(clearInteger("012")))
-	require.Equal(t, "12", string(clearInteger("0012")))
-	require.Equal(t, "10012", string(clearInteger("10012")))
-	require.Equal(t, "10012", string(clearInteger("010012")))
-	require.Equal(t, "100120", string(clearInteger("0100120")))
-	require.Equal(t, "100120", string(clearInteger("00100120")))
-	require.Equal(t, "1001200", string(clearInteger("001001200")))
+	require.Equal(t, "", clearInteger(""))
+	require.Equal(t, "0", clearInteger("0"))
+	require.Equal(t, "0", clearInteger("00"))
+	require.Equal(t, "1", clearInteger("1"))
+	require.Equal(t, "12", clearInteger("12"))
+	require.Equal(t, "12", clearInteger("012"))
+	require.Equal(t, "12", clearInteger("0012"))
+	require.Equal(t, "10012", clearInteger("10012"))
+	require.Equal(t, "10012", clearInteger("010012"))
+	require.Equal(t, "100120", clearInteger("0100120"))
+	require.Equal(t, "100120", clearInteger("00100120"))
+	require.Equal(t, "1001200", clearInteger("001001200"))
 }
 
 func TestClearFractional(t *testing.T) {
 	trailed := clearFractional("0.001001001", defaultFractionalSeparator)
-	require.Equal(t, "0.001001001", string(trailed))
+	require.Equal(t, "0.001001001", trailed)
 
 	trailed = clearFractional("0.001001010", defaultFractionalSeparator)
-	require.Equal(t, "0.00100101", string(trailed))
+	require.Equal(t, "0.00100101", trailed)
 
 	trailed = clearFractional("0.001001100", defaultFractionalSeparator)
-	require.Equal(t, "0.0010011", string(trailed))
+	require.Equal(t, "0.0010011", trailed)
 
 	trailed = clearFractional("0.001001000", defaultFractionalSeparator)
-	require.Equal(t, "0.001001", string(trailed))
+	require.Equal(t, "0.001001", trailed)
 
 	trailed = clearFractional("0.001010000", defaultFractionalSeparator)
-	require.Equal(t, "0.00101", string(trailed))
+	require.Equal(t, "0.00101", trailed)
 
 	trailed = clearFractional("0.001100000", defaultFractionalSeparator)
-	require.Equal(t, "0.0011", string(trailed))
+	require.Equal(t, "0.0011", trailed)
 
 	trailed = clearFractional("0.001000000", defaultFractionalSeparator)
-	require.Equal(t, "0.001", string(trailed))
+	require.Equal(t, "0.001", trailed)
 
 	trailed = clearFractional("0.010000000", defaultFractionalSeparator)
-	require.Equal(t, "0.01", string(trailed))
+	require.Equal(t, "0.01", trailed)
 
 	trailed = clearFractional("0.100000000", defaultFractionalSeparator)
-	require.Equal(t, "0.1", string(trailed))
+	require.Equal(t, "0.1", trailed)
 
 	trailed = clearFractional("0.000000000", defaultFractionalSeparator)
-	require.Equal(t, "", string(trailed))
+	require.Equal(t, "", trailed)
 
 	trailed = clearFractional("1.000000000", defaultFractionalSeparator)
-	require.Equal(t, "1", string(trailed))
+	require.Equal(t, "1", trailed)
 
 	trailed = clearFractional("", defaultFractionalSeparator)
-	require.Equal(t, "", string(trailed))
+	require.Equal(t, "", trailed)
 }
